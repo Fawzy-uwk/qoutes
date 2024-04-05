1526ccd5df752d6ebd5aaee0693efc88b1184c65
Fawzy Mohamed <75136326+Fawzy-uwk@users.noreply.github.com>
2024-04-05 15:55:25 +0200

    1️⃣

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    async function getAdvice() {
+      setLoading(true);
+      const res = await fetch("https://api.adviceslip.com/advice");
+      const data = await res.json();
+      console.log(data);
+      setAdvice(data);
+      setLoading(false);
+    }
+    getAdvice();
+  }, []);
+
+  const handleClick = async () => {
+    setLoading(true);
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data);
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App  h-[100vh] flex items-center justify-center p-8">
+      <div className="bg-[#323a49] w-full md:w-[30%] rounded-lg flex-col gap-6 flex items-center p-8 relative">
+        {!loading ? (
+          <>
+            <span className="uppercase font-[800] text-[14px] tracking-[.25rem] text-[#52ffa8]">
+              advice #{advice.slip?.id}
+            </span>
+            <h2 className="text-[#cee3e9] font-[700] text-center text-3xl">
+              ‘‘{advice.slip?.advice} ’’
+            </h2>
+            <img
+              src="pattern-divider-desktop.svg"
+              alt="pattern"
+              className="mb-6"
+            />
+          </>
+        ) : (
+          <div className="loader"></div>
+        )}
+        <div
+          className="dice bg-[#52ffa8] rounded-full absolute -bottom-7 cursor-pointer "
+          onClick={handleClick}
         >
-          Learn React
-        </a>
-      </header>
+          <img src="icon-dice.svg" alt="dice" className=" p-4" />
+        </div>
+      </div>
     </div>
   );
 }
